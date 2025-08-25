deae8f66692a018ac0dbc643f6be7d1ca8277d00
Jack <jack.cooper7776@gmail.com>
2025-08-25 19:32:05 +1000

    Make dmenu launch a very simple launcher using `dmenu_path`

--- modules/applications/dmenu/scripts/launch.sh
+++ modules/applications/dmenu/scripts/launch.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-
-python3 $parent_path/dmenu-launch/dmenu_launch.py --apps
+dmenu_path | dmenu -l 10 -p "Run:" "$@" | ${SHELL:-"/bin/sh"} &
